9c055014c5a8d00cda0987e160fea1283e9b70f1
filippo.grasso <grasso.filippo22@gmail.com>
2025-11-18 15:09:58 +0100

    Added Login rpc and password in users table

--- DB/user_db_init.sql
+++ DB/user_db_init.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS users
 (
     email VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     iban VARCHAR(50) NOT NULL,
     codice_fiscale VARCHAR(16) NOT NULL
 );
